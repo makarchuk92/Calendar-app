4b06505dd29a395e0900dce77aba5b7c63c3e174
Andrei <andrei.makarchuk@mail.ru>
2022-01-19 16:04:16 +0300

    Implementation Login

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Input, Button } from 'antd'
 import { rules } from './../utils/rules';
+import { useDispatch } from 'react-redux';
+import { AuthActionCreators } from './../store/reducers/action-creators';
+import { useTypedSelector } from './../hooks/useTypedSelector';
 
 
 
 const LoginForm: React.FC = () => {
+    const dispatch = useDispatch()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const {isLoading, error} = useTypedSelector(state => state.auth)
+    
     const submit = () => {
-        
+        dispatch(AuthActionCreators.Login(username, password ))
     }
 
     return (
@@ -18,7 +26,7 @@ const LoginForm: React.FC = () => {
                 name="username"
                 rules={[rules.required('Please input your username!')]}
             >
-                <Input />
+                <Input value={username} onChange={e => setUsername(e.target.value)} />
             </Form.Item>
 
             <Form.Item
@@ -26,10 +34,11 @@ const LoginForm: React.FC = () => {
                 name="password"
                 rules={[rules.required('Please input your password!')]}
             >
-                <Input.Password />
+                <Input.Password value={password} onChange={e => setPassword(e.target.value)} />
             </Form.Item>
+            { error && <div className='error' >{error}</div>}
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" loading={isLoading} >
                     Sign in
                 </Button>
             </Form.Item>
--- src/store/reducers/action-creators.ts
+++ src/store/reducers/action-creators.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { AppDispatch } from '../store';
 import { IUser } from './../../models/IUser';
 import { AuthActionEnum, SetAuthAction, SetUserAction, SetErrorAction, SetIsLoadingAction } from './authReducer';
@@ -8,6 +9,35 @@ export const AuthActionCreators = {
     setIsLoading: (payload: boolean): SetIsLoadingAction => ({type: AuthActionEnum.SET_IS_LOADING, payload}),
     setError: (payload: string): SetErrorAction => ({type: AuthActionEnum.SET_ERROR, payload}),
     Login: (username: string, password: string) => async (dispatch: AppDispatch) => {
-        
+        try {
+            dispatch(AuthActionCreators.setIsLoading(true))
+
+            setTimeout( async() => {
+                const response = await axios.get<IUser[]>('./users.json')
+                const mockUser = response.data.find(user => 
+                    user.username === username && user.password === password)
+                if (mockUser) {
+                    localStorage.setItem('auth', 'true')
+                    localStorage.setItem('username', mockUser.username)
+                    dispatch(AuthActionCreators.setIsAuth(true))
+                    dispatch(AuthActionCreators.setUser(mockUser))
+                } else {
+                    dispatch(AuthActionCreators.setError('Incorrect username or password'))
+                } 
+                dispatch(AuthActionCreators.setIsLoading(false))
+            }, 1000)
+            
+
+            
+        } catch (error) {
+            dispatch(AuthActionCreators.setError('Error trying to login'))
+        }
+    },
+    Logout: () => async(dispatch: AppDispatch) => {
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 }
