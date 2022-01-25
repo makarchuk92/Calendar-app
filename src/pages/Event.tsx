c5fc8c6f810f6d4a52c7b17dbe2f8debc465cc8b
Andrei <andrei.makarchuk@mail.ru>
2022-01-25 13:36:45 +0300

    Creating the event-modal

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EventCalendar from '../components/EventCalendar'
+import { Layout, Row, Button } from 'antd';
+import Modal from 'antd/lib/modal/Modal';
 
 
 const Event: React.FC = () => {
+    const [modalVisible, setModalVisible] = useState(false)
     return(
-        <div>
+        <Layout>
            <EventCalendar events={[]}/>
-        </div>
+           <Row justify='center'>
+                <Button onClick={() => setModalVisible(true)} type="primary"> Add event</Button>
+           </Row>
+           <Modal 
+            title="Add event"
+            visible={modalVisible} 
+            footer={null}  
+            onCancel={() => setModalVisible(false)} 
+           >
+
+           </Modal>
+        </Layout>
     )
 }
 
